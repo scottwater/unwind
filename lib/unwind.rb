780c5303013b84831f2f4e698425011f8c50d5cf
Scott Watermasysk <scottwater@gmail.com>
2012-02-14 17:49:41 -0500

    softtabs...not sure what happened

--- lib/unwind.rb
+++ lib/unwind.rb
@@ -3,63 +3,64 @@ require 'faraday'
 
 module Unwind
 
-	class TooManyRedirects < StandardError; end
-	class MissingRedirectLocation < StandardError; end
+  class TooManyRedirects < StandardError; end
+  class MissingRedirectLocation < StandardError; end
 
-	class RedirectFollower
+  class RedirectFollower
 
-		attr_reader :final_url,  :original_url, :redirect_limit, :response, :redirects
+    attr_reader :final_url,  :original_url, :redirect_limit, :response, :redirects
 
-		def initialize(original_url, limit=5)
-		 @original_url, @redirect_limit = original_url, limit
-		 @redirects = []
-		end
+    def initialize(original_url, limit=5)
+     @original_url, @redirect_limit = original_url, limit
+     @redirects = []
+     
+    end
 
-		def redirected? 
-			!(self.final_url == self.original_url)
-		end
+    def redirected? 
+      !(self.final_url == self.original_url)
+    end
 
-		def resolve(current_url=nil)
+    def resolve(current_url=nil)
 
-			ok_to_continue?
+      ok_to_continue?
 
-			current_url ||= self.original_url
-			response = Faraday.get(current_url)
+      current_url ||= self.original_url
+      response = Faraday.get(current_url)
 
-			if [301, 302, 307].include?(response.status)
-				@redirects << current_url.to_s
-				@redirect_limit -= 1
-				resolve redirect_url(response).normalize
-			else
-				@final_url = current_url.to_s
-				@response = response
-				self
-			end
-		end
+      if [301, 302, 307].include?(response.status)
+        @redirects << current_url.to_s
+        @redirect_limit -= 1
+        resolve redirect_url(response).normalize
+      else
+        @final_url = current_url.to_s
+        @response = response
+        self
+      end
+    end
 
 
-		def self.resolve(original_url, limit=5)
-			new(original_url, limit).resolve
-		end
+    def self.resolve(original_url, limit=5)
+      new(original_url, limit).resolve
+    end
 
-	private
+  private
 
-		def ok_to_continue?
-			raise TooManyRedirects if redirect_limit < 0
-		end
+    def ok_to_continue?
+      raise TooManyRedirects if redirect_limit < 0
+    end
 
-		def redirect_url(response)
-			if response['location'].nil?
-				body_match = response.body.match(/<a href=\"([^>]+)\">/i)
-				raise MissingRedirectLocation unless body_match
-				Addressable::URI.parse(body_match[0])
-			else
-				redirect_uri = Addressable::URI.parse(response['location'])
-				redirect_uri.relative? ? response.env[:url].join(response['location']) : redirect_uri
-			end
-		end
-		
+    def redirect_url(response)
+      if response['location'].nil?
+        body_match = response.body.match(/<a href=\"([^>]+)\">/i)
+        raise MissingRedirectLocation unless body_match
+        Addressable::URI.parse(body_match[0])
+      else
+        redirect_uri = Addressable::URI.parse(response['location'])
+        redirect_uri.relative? ? response.env[:url].join(response['location']) : redirect_uri
+      end
+    end
+    
 
-	end
+  end
 
 end
--- test/redirect_follower_test.rb
+++ test/redirect_follower_test.rb
@@ -3,84 +3,84 @@ require 'vcr'
 require './lib/unwind'
 
 VCR.config do |c|
-	c.stub_with :fakeweb
-	c.cassette_library_dir = 'vcr_cassettes' 
+  c.stub_with :fakeweb
+  c.cassette_library_dir = 'vcr_cassettes' 
 end
 
 describe 'Tests :)' do 
 
-	it 'should resolve the url' do 
-		VCR.use_cassette('xZVND1') do 
-			follower = Unwind::RedirectFollower.resolve('http://j.mp/xZVND1')
-			assert_equal 'http://ow.ly/i/s1O0', follower.final_url 
-			assert_equal 'http://j.mp/xZVND1', follower.original_url
-			assert_equal 2, follower.redirects.count
-			assert follower.redirected?
-		end
-	end
+  it 'should resolve the url' do 
+    VCR.use_cassette('xZVND1') do 
+      follower = Unwind::RedirectFollower.resolve('http://j.mp/xZVND1')
+      assert_equal 'http://ow.ly/i/s1O0', follower.final_url 
+      assert_equal 'http://j.mp/xZVND1', follower.original_url
+      assert_equal 2, follower.redirects.count
+      assert follower.redirected?
+    end
+  end
 
-	it 'should handle relative redirects' do 
-		VCR.use_cassette('relative stackoverflow') do 
-			follower = Unwind::RedirectFollower.resolve('http://stackoverflow.com/q/9277007/871617?stw=1')
-			assert follower.redirected?
-			assert_equal 'http://stackoverflow.com/questions/9277007/gitlabhq-w-denied-for-rails', follower.final_url
-		end
-	end
+  it 'should handle relative redirects' do 
+    VCR.use_cassette('relative stackoverflow') do 
+      follower = Unwind::RedirectFollower.resolve('http://stackoverflow.com/q/9277007/871617?stw=1')
+      assert follower.redirected?
+      assert_equal 'http://stackoverflow.com/questions/9277007/gitlabhq-w-denied-for-rails', follower.final_url
+    end
+  end
 
-	it 'should still handine relative redirects' do 
-		# http://bit.ly/A4H3a2
-		VCR.use_cassette('relative stackoverflow 2') do 
-			follower = Unwind::RedirectFollower.resolve('http://bit.ly/A4H3a2')
-			assert follower.redirected?
-		end
-	end
+  it 'should still handine relative redirects' do 
+    # http://bit.ly/A4H3a2
+    VCR.use_cassette('relative stackoverflow 2') do 
+      follower = Unwind::RedirectFollower.resolve('http://bit.ly/A4H3a2')
+      assert follower.redirected?
+    end
+  end
 
-	it 'should handle redirects to pdfs' do 
-		VCR.use_cassette('pdf') do 
-			follower = Unwind::RedirectFollower.resolve('http://binged.it/wVSFs5')
-			assert follower.redirected? 
-			assert_equal 'https://microsoft.promo.eprize.com/bingtwitter/public/fulfillment/rules.pdf', follower.final_url
-		end
-	end
+  it 'should handle redirects to pdfs' do 
+    VCR.use_cassette('pdf') do 
+      follower = Unwind::RedirectFollower.resolve('http://binged.it/wVSFs5')
+      assert follower.redirected? 
+      assert_equal 'https://microsoft.promo.eprize.com/bingtwitter/public/fulfillment/rules.pdf', follower.final_url
+    end
+  end
 
-	it 'should handle the lame amazon spaces' do 
-		VCR.use_cassette('amazon') do 
-			follower = Unwind::RedirectFollower.resolve('http://amzn.to/xrHQWS')
-			assert follower.redirected? 
-		end
-	end
+  it 'should handle the lame amazon spaces' do 
+    VCR.use_cassette('amazon') do 
+      follower = Unwind::RedirectFollower.resolve('http://amzn.to/xrHQWS')
+      assert follower.redirected? 
+    end
+  end
 
-	#http://amzn.to/xrHQWS
+  #http://amzn.to/xrHQWS
 
-	it 'should handle a https redirect' do 
-		VCR.use_cassette('ssl tpope') do 
-			follower = Unwind::RedirectFollower.resolve('http://github.com/tpope/vim-rails')
-			assert follower.redirected?
-			assert_equal 'https://github.com/tpope/vim-rails', follower.final_url
-		end
-	end
+  it 'should handle a https redirect' do 
+    VCR.use_cassette('ssl tpope') do 
+      follower = Unwind::RedirectFollower.resolve('http://github.com/tpope/vim-rails')
+      assert follower.redirected?
+      assert_equal 'https://github.com/tpope/vim-rails', follower.final_url
+    end
+  end
 
-	it 'should not be redirected' do 
-		VCR.use_cassette('no redirect') do 
-			follower  = Unwind::RedirectFollower.resolve('http://www.scottw.com')
-			assert !follower.redirected?
-		end	
-	end
+  it 'should not be redirected' do 
+    VCR.use_cassette('no redirect') do 
+      follower  = Unwind::RedirectFollower.resolve('http://www.scottw.com')
+      assert !follower.redirected?
+    end 
+  end
 
-	it 'should raise TooManyRedirects' do 
-		VCR.use_cassette('xZVND1') do 
-			follower = Unwind::RedirectFollower.new('http://j.mp/xZVND1', 1)
-			too_many_redirects = lambda {follower.resolve}
-			too_many_redirects.must_raise Unwind::TooManyRedirects
-		end
-	end
+  it 'should raise TooManyRedirects' do 
+    VCR.use_cassette('xZVND1') do 
+      follower = Unwind::RedirectFollower.new('http://j.mp/xZVND1', 1)
+      too_many_redirects = lambda {follower.resolve}
+      too_many_redirects.must_raise Unwind::TooManyRedirects
+    end
+  end
 
-	it 'should raise MissingRedirectLocation' do 
-		VCR.use_cassette('missing redirect') do 
-			follower = Unwind::RedirectFollower.new('http://tinyurl.com/6oqzkff')
-			missing_redirect_location = lambda{follower.resolve}
-			missing_redirect_location.must_raise Unwind::MissingRedirectLocation
-		end
-	end
+  it 'should raise MissingRedirectLocation' do 
+    VCR.use_cassette('missing redirect') do 
+      follower = Unwind::RedirectFollower.new('http://tinyurl.com/6oqzkff')
+      missing_redirect_location = lambda{follower.resolve}
+      missing_redirect_location.must_raise Unwind::MissingRedirectLocation
+    end
+  end
 
 end
